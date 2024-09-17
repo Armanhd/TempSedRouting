4f48cf4266b4cc8c9cab1dc9d5df7130aa9b268f
Armanhd <arman.haddadchi@gmail.com>
2024-09-17 20:19:05 +1200

    Update FallVelocity.py

--- FallVelocity.py
+++ FallVelocity.py
@@ -7,8 +7,13 @@ Created on Tue Sep 17 20:00:23 2024
 
 #%% calculate fall velocity
 def fallvelocity (di):
-    # function to calculate fall velocity
-    # input is diameter in meter [m]
+    # function to calculate fall velocity using
+    # input is sediment diameter in meter [m]
+    
+    Rrho=1.65       # R=((rohs/rho)-1),rhos=2650, rho=1000 - no unit
+    nu=0.0000010533 # kinematic viscosity [m^2/s]
+    g=9.805         # acceleration gravity [m/s^2]
+    
     distar=(((Rrho*g)/(nu**2))**(1/3))*(di)
     falveli= (nu/di)*(((25+(1.2*(distar**2)))**(0.5))-5)**(1.5)     # fall velocity in [m/s]
     print ('fall velocity(s) are:', falveli)
